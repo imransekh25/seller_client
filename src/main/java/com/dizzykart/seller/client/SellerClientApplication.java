d2f902179b10273bb687ebd1e5449b643529cf9c
imran <imran>
2023-05-15 00:15:59 +0530

    first commit

---
+++ src/main/java/com/dizzykart/seller/client/SellerClientApplication.java
@@ -0,0 +1,13 @@
+package com.dizzykart.seller.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellerClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dizzykart/seller/client/SellerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizzykart.seller.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
